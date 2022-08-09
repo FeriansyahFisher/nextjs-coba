c39d474c0566d5574aeb533ec9ac5c914f3ee9fb
Feriansyah Fisher <feriansav@gmail.com>
2022-08-09 09:13:22 +0700

    polish and add link in index blog post

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import Link from 'next/link';
+import Date from '../components/date';
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
+
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -15,6 +18,9 @@ export async function getStaticProps() {
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
+      <Head>
+        <title>Next js Coba</title>
+      </Head>
       {/* Keep the existing code here */}
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
@@ -30,11 +36,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>

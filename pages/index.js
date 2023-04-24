b9e1756740cddd3111ba8ef5a6efc9d470104612
Feriansyah <feriansav@gmail.com>
2023-04-24 10:14:14 +0700

    saving last modified

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 
-
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
